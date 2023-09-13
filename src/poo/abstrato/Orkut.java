8f888e470681fa0c22da314cf19605e4e35fc091
divaprates <divasilvaprates@gmail.com>
2023-09-12 21:20:28 -0300

    feat: Abstração

---
+++ src/poo/abstrato/Orkut.java
@@ -0,0 +1,15 @@
+package poo.abstrato;
+
+public class Orkut extends Mensagem{
+
+    @Override
+    public void enviarMensagem() {
+        System.out.println("Mensagem enviada pelo Orkut");
+    }
+
+    @Override
+    public void receberMensagem() {
+        System.out.println("Mensagem recebida pelo Orkut");
+    }
+    
+}
